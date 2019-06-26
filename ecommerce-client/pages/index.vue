035935adf3a59159acaff48f6fd6a6a7ed20b9ad
Abdulrahman Faid <abdulrahman@localhost.localdomain>
2019-06-26 15:59:47 -0400

    Install Structure files

---
+++ ecommerce-client/pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container">
+    <div>
+      <h1 class="title">
+        ecommerce-client
+      </h1>
+    </div>
+  </div>
+</template> 
+<script>
+export default {
+  
+}
+</script>
