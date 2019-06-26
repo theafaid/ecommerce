8a5ea83f66198f03ee9f0354d6cf6238cc970c00
Abdulrahman Faid <abdulrahman@localhost.localdomain>
2019-06-26 16:41:25 -0400

    Setup navbar

--- ecommerce-client/layouts/default.vue
+++ ecommerce-client/layouts/default.vue
@@ -1,5 +1,16 @@
 <template>
   <div>
+    <navbar></navbar>
     <nuxt />
   </div>
 </template>
+
+<script>
+    import Navbar from "./partials/navbar"
+
+    export default {
+      components: {
+        'navbar': Navbar
+      }
+    }
+</script>
